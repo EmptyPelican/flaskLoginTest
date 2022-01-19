17b4ea6f11a47c176fd24689b1a451b17568d1d1
EmptyPelican <theemptypelican@gmail.com>
2022-01-19 12:27:39 -0600

    finsihed project, added authenticationt o some pages

--- auth.py
+++ auth.py
@@ -2,6 +2,7 @@ from flask import (Blueprint, render_template,
                    redirect, url_for, request,
                    flash)
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user
 from .models import User
 from . import db
 
@@ -12,6 +13,27 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login')
 def login():
     return render_template('login.html')
+
+
+# new route for handling data submitted with POST
+@auth.route('/login', methods=['POST'])
+def login_post():
+    # verify if the user has the correct credentials
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if request.form.get('remember'):
+        remember = True
+    else:
+        remember = False
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again')
+        return redirect(url_for('auth.login'))
+
+    login_user(user, remember=remember)
+    return redirect(url_for('main.profile'))
 
 
 @auth.route('/signup')
@@ -40,5 +62,7 @@ def signup_post():
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return 'Logout'
+    logout_user()
+    return redirect(url_for('main.index'))
